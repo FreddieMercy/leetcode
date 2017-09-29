0f4e18b7b41303c95dd5d78f49c38f4690943b32
FreddieMercy <junhao.zhang.freddie@gmail.com>
2017-09-28 23:56:25 -0700

    Two very hard questions

---
+++ Java/src/_2017/June2017/Jun20th2017/_642DesignSearchAutocompleteSystem/AutocompleteSystem.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package _2017.June2017.Jun20th2017._642DesignSearchAutocompleteSystem;
+import java.lang.*;
+import java.util.*;
+/**
+ *
+ * @author junhaozhang
+ */
+public class AutocompleteSystem {
+    public AutocompleteSystem(String[] sentences, int[] times) {
+        
+    }
+    
+    public List<String> input(char c) {
+        return new ArrayList();
+    }
+}
+
+/**
+ * Your AutocompleteSystem object will be instantiated and called as such:
+ * AutocompleteSystem obj = new AutocompleteSystem(sentences, times);
+ * List<String> param_1 = obj.input(c);
+ */
