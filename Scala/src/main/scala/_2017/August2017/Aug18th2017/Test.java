1e10d32f7ca7235cfff168ebaee47ffd895205fa
FreddieMercy <junhao.zhang.freddie@gmail.com>
2017-10-01 18:57:12 -0700

    Added Scala

---
+++ Scala/src/main/scala/_2017/August2017/Aug18th2017/Test.java
@@ -0,0 +1,13 @@
+package _2017.August2017.Aug18th2017;
+
+public class Test {
+    object test
+    {
+        def printMe( ) : Unit = {
+        {
+            println("Freddie");
+
+        }
+    }
+}
+
