f2b1cf171b9055e972cea86d10ebb7be36342851
jzhang1 <junhao.zhang.freddie@gmail.com>
2018-05-28 14:49:02 -0700

    Go config?

--- Go/_2018/January2018/Jan05th2018/_581ShortestUnsortedContinuousSubarray/go581ShortestUnsortedContinuousSubarray.go
+++ Go/_2018/January2018/Jan05th2018/_581ShortestUnsortedContinuousSubarray/go581ShortestUnsortedContinuousSubarray.go
@@ -1,25 +1,12 @@
 package go581ShortestUnsortedContinuousSubarray
 
+import (
+	"github.com/golang-collections/collections/stack"
+)
+
 func findUnsortedSubarray(nums []int) int {
-	if len(nums) <= 1 {
-		return 0
-	}
+	var ans int = 0
+	stack := new(Stack)
 
-	i := 0
-	var j int = len(nums) - 1
-	var ans int = len(nums)
-
-	for i < j && nums[i] <= nums[i+1] {
-		i++
-		ans -= 1
-	}
-
-	for i < j && nums[i] <= nums[j] && nums[j] >= nums[j-1] {
-		j--
-		ans -= 1
-	}
-	if i == j && nums[i] <= nums[j] {
-		ans--
-	}
 	return ans
 }
