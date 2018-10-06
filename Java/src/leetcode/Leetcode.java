6c780f2ecfda18b55511225683919d8a92daf873
FreddieMercy <junhao.zhang.freddie@gmail.com>
2018-10-06 01:33:58 -0700

    _291WordPatternII is art piece

--- Java/src/leetcode/Leetcode.java
+++ Java/src/leetcode/Leetcode.java
@@ -43,11 +43,52 @@ public class Leetcode {
     /**
      * @param args the command line arguments
      */
+    // Complete the substrCount function below.
+    private static boolean isValid(String s, int left, int right, boolean[][] dp){
+
+        if(left==right){
+            dp[left][right] = true;
+            return true;
+        }
+        if(s.charAt(left)==s.charAt(right)){
+            int mid = (right-left)/2+left;
+            if((right-left)%2==1){//even
+                dp[left][right] = (dp[left][mid]&&dp[mid+1][right]);
+                return dp[left][right];
+            }
+            else{
+                if(dp[left][mid-1]&&dp[mid+1][right]){
+                    dp[left][right] = (s.charAt(mid) == s.charAt(left));
+                    return true;
+                }
+                return false;
+            }
+        }
+        return false;
+    }
+    static long substrCount(int n, String s) {
+
+        if(s==null||n<0){
+            return 0;
+        }
+
+        boolean[][] dp = new boolean[n][n];
+        long ans = 0;
+        for(int len = 0; len<n;++len){
+            for(int i = 0; i < n - len;++i){
+                if(isValid(s, i, i+len, dp)){
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
     public static void main(String[] args) {
 
-        Solution x= new Solution();
 
-        System.out.println(x.minCostII(new int[][]{{8,9},{8,9},{8,9},{8,9}}));
+        System.out.println(substrCount(727310,
+                "ccacacabccacabaaaabbcbccbabcbbcaccabaababcbcacabcabacbbbccccabcbcabbaaaaabacbcbbbcababaabcbbaaababababbabcaabcaacacbbaccbbabbcbbcbacbacabaaaaccacbaabccabbacabaabaaaabbccbaaaabacabcacbbabbacbcbccbbbaaabaaacaabacccaacbcccaacbbcaabcbbccbccacbbcbcaaabbaababacccbacacbcbcbbccaacbbacbcbaaaacaccbcaaacbbcbbabaaacbaccaccbbabbcccbcbcbcbcabbccbacccbacabcaacbcaccabbacbbccccaabbacccaacbbbacbccbcaaaaaabaacaaabccbbcccaacbacbccaaacaacaaaacbbaaccacbcbaaaccaabcbccacaaccccacaacbcacccbcababcabacaabbcacccbacbbaaaccabbabaaccabbcbbcaabbcabaacabacbcabbaaabccabcacbcbabcbccbabcabbbcbacaaacaabbbabbaacbbacaccccabbabcbcabababbcbaaacbaacbacacbabbcacccbccbbbcbcabcabbbcaabbaccccabaabbcbcccabaacccccaaacbbbcbcacacbabaccccbcbabacaaaabcccaaccacbcbbcccaacccbbcaaaccccaabacabcabbccaababbcabccbcaccccbaaabbbcbabaccacaabcabcbacaccbaccbbaabccbbbccaccabccbabbbccbaabcaabcabcbbabccbaaccabaacbbaaaabcbcabaacacbcaabbaaabaaccacbaacababcbacbaacacccacaacbacbbaacbcbbbabccbababcbcccbccbcacccbababbcacaaaaacbabcabcacaccabaabcaaaacacbcc"));
 
 
     }
